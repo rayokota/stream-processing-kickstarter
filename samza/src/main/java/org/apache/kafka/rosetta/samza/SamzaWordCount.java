b2fbebcc9e22366f1ee1018c7edfd68d05bd3032
Robert Yokota <rayokota@gmail.com>
2018-11-27 21:13:00 -0800

    Upgrade Samza to 1.0

--- samza/src/main/java/org/apache/kafka/rosetta/samza/SamzaWordCount.java
+++ samza/src/main/java/org/apache/kafka/rosetta/samza/SamzaWordCount.java
@@ -18,13 +18,11 @@
 
 package org.apache.kafka.rosetta.samza;
 
-import java.time.Duration;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import org.apache.kafka.rosetta.WordCount;
 import org.apache.samza.application.StreamApplication;
+import org.apache.samza.application.descriptors.StreamApplicationDescriptor;
 import org.apache.samza.config.Config;
 import org.apache.samza.config.JobConfig;
 import org.apache.samza.config.JobCoordinatorConfig;
@@ -34,16 +32,23 @@ import org.apache.samza.container.grouper.task.SingleContainerGrouperFactory;
 import org.apache.samza.operators.KV;
 import org.apache.samza.operators.MessageStream;
 import org.apache.samza.operators.OutputStream;
-import org.apache.samza.operators.StreamGraph;
 import org.apache.samza.operators.triggers.Triggers;
 import org.apache.samza.operators.windows.AccumulationMode;
 import org.apache.samza.operators.windows.Windows;
 import org.apache.samza.runtime.LocalApplicationRunner;
+import org.apache.samza.serializers.IntegerSerde;
 import org.apache.samza.serializers.KVSerde;
 import org.apache.samza.serializers.LongSerde;
 import org.apache.samza.serializers.StringSerde;
-import org.apache.samza.standalone.PassthroughCoordinationUtilsFactory;
 import org.apache.samza.standalone.PassthroughJobCoordinatorFactory;
+import org.apache.samza.system.kafka.descriptors.KafkaInputDescriptor;
+import org.apache.samza.system.kafka.descriptors.KafkaOutputDescriptor;
+import org.apache.samza.system.kafka.descriptors.KafkaSystemDescriptor;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SamzaWordCount implements WordCount {
 
@@ -56,22 +61,11 @@ public class SamzaWordCount implements WordCount {
 
     configs.put(JobConfig.JOB_NAME(), "word-count");
     configs.put(JobConfig.PROCESSOR_ID(), "1");
-    configs.put(
-        JobCoordinatorConfig.JOB_COORDINATION_UTILS_FACTORY,
-        PassthroughCoordinationUtilsFactory.class.getName()
-    );
-    configs.put(
-        JobCoordinatorConfig.JOB_COORDINATOR_FACTORY,
+    configs.put(JobCoordinatorConfig.JOB_COORDINATOR_FACTORY,
         PassthroughJobCoordinatorFactory.class.getName()
     );
-    configs.put(
-        TaskConfig.GROUPER_FACTORY(),
-        SingleContainerGrouperFactory.class.getName()
-    );
-    configs.put(
-        "systems.kafka.samza.factory",
-        "org.apache.samza.system.kafka.KafkaSystemFactory"
-    );
+    configs.put(TaskConfig.GROUPER_FACTORY(), SingleContainerGrouperFactory.class.getName());
+    configs.put("systems.kafka.samza.factory", "org.apache.samza.system.kafka.KafkaSystemFactory");
     configs.put("systems.kafka.producer.bootstrap.servers", bootstrapServers);
     configs.put("systems.kafka.consumer.zookeeper.connect", zookeeperConnect);
     configs.put("systems.kafka.samza.offset.default", "oldest");
@@ -79,8 +73,10 @@ public class SamzaWordCount implements WordCount {
     configs.put("job.default.system", "kafka");
 
     LocalApplicationRunner runner =
-        new LocalApplicationRunner(new MapConfig(configs));
-    runner.run(new WordCountApplication(inputTopic, outputTopic));
+        new LocalApplicationRunner(
+            new WordCountApplication(bootstrapServers, zookeeperConnect, inputTopic, outputTopic),
+            new MapConfig(configs));
+    runner.run();
     runner.waitForFinish();
   }
 
@@ -88,39 +84,70 @@ public class SamzaWordCount implements WordCount {
   public void close() {
   }
 
+
   static class WordCountApplication implements StreamApplication {
 
+    private static final String KAFKA_SYSTEM_NAME = "kafka";
+    private static final Map<String, String> KAFKA_DEFAULT_STREAM_CONFIGS = ImmutableMap.of(
+        "replication.factor",
+        "1"
+    );
+
+    private String bootstrapServers;
+    private String zookeeperConnect;
     private String inputTopic;
     private String outputTopic;
 
-    public WordCountApplication(String inputTopic, String outputTopic) {
+    public WordCountApplication(
+        String bootstrapServers, String zookeeperConnect, String inputTopic, String outputTopic
+    ) {
+      System.out.println("*** boot " + bootstrapServers);
+      System.out.println("*** zooww " + zookeeperConnect);
+      this.bootstrapServers = bootstrapServers;
+      this.zookeeperConnect = zookeeperConnect;
       this.inputTopic = inputTopic;
       this.outputTopic = outputTopic;
     }
 
     @Override
-    public void init(StreamGraph graph, Config config) {
-      MessageStream<KV<String, String>> words =
-          graph.getInputStream(
-              inputTopic, KVSerde.of(new StringSerde(), new StringSerde()));
-      OutputStream<KV<String, Long>> counts =
-          graph.getOutputStream(
-              outputTopic, KVSerde.of(new StringSerde(), new LongSerde()));
+    public void describe(StreamApplicationDescriptor streamApplicationDescriptor) {
+      KafkaSystemDescriptor kafkaSystemDescriptor =
+          new KafkaSystemDescriptor(KAFKA_SYSTEM_NAME)
+              .withConsumerZkConnect(ImmutableList.of(zookeeperConnect))
+              .withProducerBootstrapServers(ImmutableList.of(bootstrapServers))
+              .withDefaultStreamConfigs(KAFKA_DEFAULT_STREAM_CONFIGS);
 
-      words.flatMap(kv -> Arrays.asList(kv.value.toLowerCase().split("\\W+")))
-          .window(Windows.keyedTumblingWindow(
-              (String kv) -> kv, Duration.ofSeconds(1),
-              () -> 0L, (m, prevCount) -> prevCount + 1,
-              new StringSerde(), new LongSerde()
-          )
-              .setEarlyTrigger(Triggers.repeat(Triggers.count(1)))
-              .setAccumulationMode(AccumulationMode.ACCUMULATING), "count")
-          .map(windowPane -> {
-            String word = windowPane.getKey().getKey();
-            long count = windowPane.getMessage();
-            return KV.of(word, count);
+      KafkaInputDescriptor<KV<String, String>> inputDescriptor =
+          kafkaSystemDescriptor.getInputDescriptor(inputTopic,
+          KVSerde.of(new StringSerde(), new StringSerde())
+      );
+      KafkaOutputDescriptor<KV<String, Long>> outputDescriptor =
+          kafkaSystemDescriptor.getOutputDescriptor(outputTopic,
+          KVSerde.of(new StringSerde(), new LongSerde())
+      );
+
+      MessageStream<KV<String, String>> lines = streamApplicationDescriptor.getInputStream(
+          inputDescriptor);
+      OutputStream<KV<String, Long>> counts = streamApplicationDescriptor.getOutputStream(
+          outputDescriptor);
+
+      lines.map(kv -> kv.value)
+          .flatMap(s -> {
+            return Arrays.asList(s.toLowerCase().split("\\W+"));
           })
+          .window(Windows.keyedSessionWindow(w -> w,
+              Duration.ofSeconds(1),
+              () -> 0L,
+              (m, prevCount) -> prevCount + 1,
+              new StringSerde(),
+              new LongSerde()
+          ), "count")
+          .map(windowPane -> KV.of(windowPane.getKey().getKey(),
+              windowPane.getMessage()
+          ))
           .sendTo(counts);
     }
   }
+
+
 }
